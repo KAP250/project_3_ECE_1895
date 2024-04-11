735f3fb9da48e7fa7d2389319b51cd77bf07ad09
Krishna <Krishna6303@gmail.com>
2024-04-10 20:05:42 -0400

    Working on cam

--- camera.py
+++ camera.py
@@ -1,6 +1,6 @@
 import cv2
 
-video_capture = cv2.VideoCapture(0)
+video_capture = cv2.VideoCapture('v4l2src device=/dev/video0 ! video/x-raw, width=(int)720, height=(int)1080 ! videoconvert ! video/x-raw, format=(string)BGR ! appsink', cv2.CAP_GSTREAMER)
 while True:
     # Capture frame-by-frame
     ret, frame = video_capture.read()
